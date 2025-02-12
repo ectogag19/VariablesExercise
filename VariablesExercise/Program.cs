a122bcb4cf2341da5b67b94dd608efe4eb00822c
George Garcia <georgegarcia05012001@gmail.com>
2025-02-12 13:40:18 -0600

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "George";
+            int myAge = 23;
+            char myGender = 'M';
+            bool isAStudent = true;
+            double myWeight = 145.8;
+            decimal mySalary = 2000.00m;
+            
+            Console.WriteLine($"Name: {myName}\nAge: {myAge}\nGender: {myGender}\nIs a Student: {isAStudent}\nWeight: {myWeight}\nSalary: {mySalary}");
+
+
         }
     }
 }
